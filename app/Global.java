9f4e795b714f8789dea1f75c74f06e3981e6536c
kayjeelahong <kagiso.killagram@gmail.com>
2015-11-16 07:23:37 +0200

    git remote add origin https://github.com/kayjeee/PlayMongoDemo-master.git git push -u origin master
    Signed-off-by: kayjeelahong <kagiso.killagram@gmail.com>

---
+++ app/Global.java
@@ -0,0 +1,41 @@
+
+import java.net.UnknownHostException;
+
+import play.GlobalSettings;
+import play.Logger;
+
+import com.google.code.morphia.Morphia;
+import com.mongodb.Mongo;
+
+import controllers.MorphiaObject;
+
+public class Global extends GlobalSettings {
+
+@Override
+	public void onStart(play.Application arg0) {
+		super.beforeStart(arg0);
+		Logger.debug("** onStart **"); 
+		try {
+			Configuration config = Configuration.root().getConfig("jongo");
+			String host = config.getString("mongodb.host");
+		
+			   String uri =config.getString("mongodb.uri");
+		  
+			   System.out.print(uri);
+			   System.out.print(host);
+			MorphiaObject.mongo = new Mongo("blrwazxweuc8du7-mongodb.services.clever-cloud.com");
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+		Configuration config = Configuration.root().getConfig("jongo");
+	      
+     
+     
+		MorphiaObject.morphia = new Morphia();
+		MorphiaObject.datastore = MorphiaObject.morphia.createDatastore(MorphiaObject.mongo, "test");
+		MorphiaObject.datastore.ensureIndexes();   
+		MorphiaObject.datastore.ensureCaps();  
+
+		Logger.debug("** Morphia datastore: " + MorphiaObject.datastore.getDB());
+	}
+}
